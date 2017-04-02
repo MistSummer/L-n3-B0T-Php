044bee906e7d72101b6a732c28b94dfcf35d4509
MistSummer <zbeargx@gmail.com>
2017-04-02 23:09:16 +0700

    Update csvtest.php

--- csvtest.php
+++ csvtest.php
@@ -1,7 +1,3 @@
 <?php
-
-$arrayFromCSV =  array_map('str_getcsv', 'https://smooth-winter-3239.herokuapp.com/test.csv');
-
+$arrayFromCSV =  array_map('str_getcsv', file('https://smooth-winter-3239.herokuapp.com/test.csv'));
 print_r($arrayFromCSV);
-
-?>
