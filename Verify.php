2748958b4062d00c01276723a4794b77289ef6dc
MistSummer <zbeargx@gmail.com>
2017-04-02 10:53:35 +0700

    Update Verify.php

--- Verify.php
+++ Verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = 'GKKuFc6FcuRQG+7Slc8coI6X3CFubcL4fvfZTAot6lzD8ahVOeyJOF97pObyGon1Je61dHpXbFA/SXjNwJ+50ZEeDQS0dqSs5dkDY/vi6GAGUDmb5bbkFK5N0OQf22jQiyaQcMtsY7t/KSOpm6Ib7wdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
