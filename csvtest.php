e3721676b6f2d304f57c86421f4db5712de5e543
MistSummer <zbeargx@gmail.com>
2017-04-02 23:14:42 +0700

    Update csvtest.php

--- csvtest.php
+++ csvtest.php
@@ -1,3 +1,35 @@
 <?php
-$arrayFromCSV =  array_map('str_getcsv', file('https://smooth-winter-3239.herokuapp.com/test.csv'));
-print_r($arrayFromCSV);
+
+function parse_csv_file($csvfile) {
+    $csv = Array();
+    $rowcount = 0;
+    if (($handle = fopen($csvfile, "r")) !== FALSE) {
+        $max_line_length = defined('MAX_LINE_LENGTH') ? MAX_LINE_LENGTH : 10000;
+        $header = fgetcsv($handle, $max_line_length);
+        $header_colcount = count($header);
+        while (($row = fgetcsv($handle, $max_line_length)) !== FALSE) {
+            $row_colcount = count($row);
+            if ($row_colcount == $header_colcount) {
+                $entry = array_combine($header, $row);
+                $csv[] = $entry;
+            }
+            else {
+                error_log("csvreader: Invalid number of columns at line " . ($rowcount + 2) . " (row " . ($rowcount + 1) . "). Expected=$header_colcount Got=$row_colcount");
+                return null;
+            }
+            $rowcount++;
+        }
+        //echo "Totally $rowcount rows found\n";
+        fclose($handle);
+    }
+    else {
+        error_log("csvreader: Could not read CSV \"$csvfile\"");
+        return null;
+    }
+    return $csv;
+}
+
+$csvFile = 'https://smooth-winter-3239.herokuapp.com/test.csv';
+$csv = parse_csv_file($csvFile);
+
+print_r($csv);
