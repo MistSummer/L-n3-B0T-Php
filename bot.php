f3769165d3a6573dfc0e04890a5f765ff3bf3e4f
MistSummer <zbeargx@gmail.com>
2017-04-03 08:09:52 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,4 +1,38 @@
 <?php
+
+//Function
+
+$thai_day_arr=array("อาทิตย์","จันทร์","อังคาร","พุธ","พฤหัสบดี","ศุกร์","เสาร์");
+$thai_month_arr=array(
+    "0"=>"",
+    "1"=>"มกราคม",
+    "2"=>"กุมภาพันธ์",
+    "3"=>"มีนาคม",
+    "4"=>"เมษายน",
+    "5"=>"พฤษภาคม",
+    "6"=>"มิถุนายน", 
+    "7"=>"กรกฎาคม",
+    "8"=>"สิงหาคม",
+    "9"=>"กันยายน",
+    "10"=>"ตุลาคม",
+    "11"=>"พฤศจิกายน",
+    "12"=>"ธันวาคม"                 
+);
+function thai_date($time){
+    global $thai_day_arr,$thai_month_arr;
+    $thai_date_return="วัน".$thai_day_arr[date("w",$time)];
+    $thai_date_return.= "ที่ ".date("j",$time);
+    $thai_date_return.=" เดือน".$thai_month_arr[date("n",$time)];
+    $thai_date_return.= " พ.ศ.".(date("Yํ",$time)+543);
+    $thai_date_return.= "  ".date("H:i",$time)." น.";
+    return $thai_date_return;
+}
+
+
+
+
+
+//::: LINE SECTION :::
 $access_token = 'GKKuFc6FcuRQG+7Slc8coI6X3CFubcL4fvfZTAot6lzD8ahVOeyJOF97pObyGon1Je61dHpXbFA/SXjNwJ+50ZEeDQS0dqSs5dkDY/vi6GAGUDmb5bbkFK5N0OQf22jQiyaQcMtsY7t/KSOpm6Ib7wdB04t89/1O/w1cDnyilFU=';
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -19,8 +53,10 @@ if (!is_null($events['events'])) {
 						
 			if (strncmp($text, "เมี๊ยว", 6) === 0){$textout = "ร้องเรียกเมี๊ยวๆ เดี๋ยวก็มา";}
 			else if (strncmp($text, "ขอหวย", 5) === 0){
-				//$textout =  rand(100000,999999)  ;	
-				$textout = "เลขนำโชคของคุณคือ ";
+				//$textout =  rand(100000,999999)  ;
+				$eng_date=time();
+				$thaidate = thai_date($eng_date);
+				$textout = "เลขนำโชคประจำ".thai_date($eng_date)." ของคุณคือ ";
 				for ($i = 0; $i<6; $i++) 
 				{
 					$textout .= mt_rand(0,9);
